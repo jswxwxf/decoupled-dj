1ed2469890c9de43ad9cfda454932cbc6dc43a70
jeffreywang <wxwxf@hotmail.com>
2022-08-25 18:04:54 +0800

    add next

--- next-blog/pages/posts/[id].tsx
+++ next-blog/pages/posts/[id].tsx
@@ -1,6 +1,6 @@
 import { GetServerSideProps } from "next";
 
-const BASE_URL = "http://localhost:8000/blog/api";
+const BASE_URL = "http://127.0.0.1:8000/blog/api";
 
 // export const getStaticPaths: GetStaticPaths = async (_) => {
 //   const res = await fetch(`${BASE_URL}/posts/`);
